341c0206ddbde076d90f397fca2ea5fbe1c34db6
Silhm <1029845+Silhm@users.noreply.github.com>
2018-03-21 12:00:18 +0100

    Recall soundcard state

--- soundCardState.py
+++ soundCardState.py
@@ -18,7 +18,13 @@ class State:
 
     def __init__(self, ipAddr=None, port=None ):
         self.ipAddr = ipAddr
-        self._chanData = False
+        
+        midiPort = mido.get_output_names()[0]
+        self.midiOUT = mido.open_output(midiPort)
+
+        self._chanData = False       
+        self.stripCount = 8
+
     
     def getAll(self):
         """
@@ -38,111 +44,119 @@ class State:
         Set the controller in the same state as the interface
         """
         print("refreshing controller state")
-        #TODO: handle all the data once I got to know the datastore format
+
+        if self._chanData:
+            self.recalFader()
+            self.recalSolo()
+            self.recalMute()
+
+        else:
+            print(">> No data <<")
+
+
+    def recalFader(self, fId=False):
+        """
+        Recal position of one fader or all if no id provided
+        """
+        if fId:
+            chanFader = "mix/chan/{}/matrix/fader".format(fId)
+            faderVal = self._chanData[chanFader]
+            self._dispatchFader(faderVal, fId)
+        else:
+            for i in range(0,self.stripCount):
+                chanFader = "mix/chan/{}/matrix/fader".format(i)
+                faderVal = self._chanData[chanFader]
+                self._dispatchFader(faderVal, i)
+ 
+
+    def recalSolo(self, sId=False):
+        """
+        Recal state of button line 1 or all if no id provided
+        """
+        if sId:
+            buttonId = "mix/chan/{}/matrix/solo".format(sId)
+            buttonState = self._chanData[buttonId]
+            self._dispatchButtonsLine1(buttonState, sId)
+        else :
+            for i in range(0,self.stripCount):
+                buttonId = "mix/chan/{}/matrix/solo".format(i)
+                buttonState = self._chanData[buttonId]
+                self._dispatchButtonsLine1(buttonState, i)
+
+
+    def recalMute(self, sId=False):
+        """
+        Recal state of button line 2 or all if no id provided
+        """
+        if sId:
+            buttonId = "mix/chan/{}/matrix/mute".format(sId)
+            buttonState = self._chanData[buttonId]
+            self._dispatchButtonsLine2(buttonState, sId)
+        else :
+            for i in range(0,self.stripCount):
+                buttonId = "mix/chan/{}/matrix/mute".format(i)
+                buttonState = self._chanData[buttonId]
+                self._dispatchButtonsLine2(buttonState, i)
+
+
+
+
 
 
         
-    def _dispatchFader(self, address, stripId, faderValue):
+    def _dispatchFader(self, faderValue, stripId):
         """
         Convert fader OSC value to MIDI value
         """
-        faderMidiRange = self.ctrlConfig.getFaderMidiRange()
-        faderOSCRange = self.dawConfig.getFaderOSCRange()
-        faderMove = self.ctrlConfig.getFaderMove("type")
-        readyVal = convertValueToMidiRange(faderValue, self.dawConfig.getFaderOSCRange(), self.ctrlConfig.getFaderMidiRange())
+        apiRange = [0,4]
+        midiRange = [-8192,8176]
+        readyVal = convertValueToMidiRange(faderValue, apiRange, midiRange)
         
         # TODO: handle bank (should be available in database or memory)
-        # stripId with bank handle
-        bank = self.db.getCurrentBank()
-        bankSize = self.db.getBankSize()
-        
-        sId = stripId
 
-        # need to stay in 1 -> bankSize range
-        if(sId > bankSize):
-            sId = (sId % bankSize) +1
-
-
-        midiMessage = "{} ch: {} value:{}".format(faderMove, sId, readyVal)
-        print("Dispatching OSC: {} {} {} to MIDI: {}  ".format(address,stripId,faderValue, midiMessage))
-        
-        msg = mido.Message('pitchwheel', pitch=readyVal, channel=sId)
+        midiMessage = "{} ch: {} value:{}".format("pitchwheel", stripId, readyVal)
+        msg = mido.Message('pitchwheel', pitch=readyVal, channel=stripId)
         self.midiOUT.send(msg)
+        print("[{}][fader:{}] [midi OUT] : {} ".format(stripId, faderValue, msg))
 
 
-    def _dispatchButtonsLine1(self, address, stripId, buttonValue):
+    def _dispatchButtonsLine1(self,  buttonValue, stripId):
         """
         Convert Solo / Rec OSC value to MIDI value
         """
-        # Do nothing if not good mode
-        buttonMode = self.db.getButtonMode()
-        bank = self.db.getCurrentBank()
-        bankSize = self.db.getBankSize()
-
-        if buttonMode == "solomute" and "rec" in address:
-            return
-    
-        line = 1
-        buttonsMidiNotes  = self.ctrlConfig.getButtonNotes(line)
-        buttonsMidiType = self.ctrlConfig.getButtonType(line)
-
-        sId = stripId -1
-        # need to stay in 1 -> bankSize range
-        if(sId >= bankSize):
-            sId = (sId % bankSize) 
-
-        midiNote = midiFullNoteToNumber(buttonsMidiNotes[sId])
-        midiVelocity = 127 #buttonsMidiValueOn if buttonValue else buttonsMidiValueOff
-        msg = mido.Message(buttonsMidiType, note=midiNote, velocity=midiVelocity)
-        print("Dispatching OSC: {} {} {} to MIDI: {}  ".format(address,stripId,buttonValue, msg))
+        soloMidiNotes = ["E0", "F0", "F#0", "G0", "G#0", "A0", "A#0", "B0"]
+        
+        midiNote = midiFullNoteToNumber(soloMidiNotes[stripId])
+        midiVelocity = 127 if buttonValue else 0
+        msg = mido.Message("note_on", note=midiNote, velocity=midiVelocity)
         self.midiOUT.send(msg)
+        print("[{}][solo:{}] [midi OUT] : {} ".format(stripId, bool(buttonValue), msg))
 
 
-    def _dispatchButtonsLine2(self, address, stripId, buttonValue):
+    def _dispatchButtonsLine2(self,  buttonValue, stripId):
         """
         Convert Mute / Select OSC value to MIDI value
         """
-        buttonMode = self.db.getButtonMode()
-        bank = self.db.getCurrentBank()
-        bankSize = self.db.getBankSize()
-
-        # Do nothing if not good mode
-        if buttonMode == "solomute" and "select" in address:
-            return
+        muteMidiNotes = ["C1", "C#1", "D1", "D#1", "E1", "F1", "F#1", "G1"]
         
-        line = 2
-        buttonsMidiNotes  = self.ctrlConfig.getButtonNotes(line)
-        buttonsMidiType = self.ctrlConfig.getButtonType(line)
+        midiNote = midiFullNoteToNumber(muteMidiNotes[stripId])
+        midiVelocity = 127 if buttonValue else 0
+        msg = mido.Message("note_on", note=midiNote, velocity=midiVelocity)
+        self.midiOUT.send(msg)
+        print("[{}][mute:{}] [midi OUT] : {} ".format(stripId, bool(buttonValue), msg))
+
  
-        sId = stripId - 1
-        # need to stay in 1 -> bankSize range
-        if(sId >= bankSize):
-            sId = (sId % bankSize) 
-
-        midiNote = midiFullNoteToNumber(buttonsMidiNotes[sId])
-
-        midiVelocity = 127 #buttonsMidiValueOn if buttonValue else buttonsMidiValueOff
-        msg = mido.Message(buttonsMidiType, note=midiNote, velocity=midiVelocity)
-        print("Dispatching OSC: {} {} {} to MIDI: {}  ".format(address,stripId,buttonValue, msg))
-        self.midiOUT.send(msg)
-
-
-    def _dispatchFunctionButtons(self, address, bname):
+    def _dispatchButtonsLine2(self,  buttonValue, stripId):
         """
         Convert Mute / Select OSC value to MIDI value
         """
-        bname = bname[0]
+        muteMidiNotes = ["C1", "C#1", "D1", "D#1", "E1", "F1", "F#1", "G1"]
         
-        fNote  = midiFullNoteToNumber(self.ctrlConfig.getfButtonNote(bname,"note"))
-        fVelocity = self.ctrlConfig.getfButtonNote(bname,"valueOn")
-        fChannel = self.ctrlConfig.getfButtonNote(bname,"ch")
-        fType = self.ctrlConfig.getfButtonNote(bname,"type")
-
-        msg = mido.Message(fType, note=fNote, velocity=fVelocity, channel=fChannel)
-        print("Dispatching OSC: {} (mapped to {}) to MIDI: {}  ".format(address,bname, msg))
+        midiNote = midiFullNoteToNumber(muteMidiNotes[stripId])
+        midiVelocity = 127 if buttonValue else 0
+        msg = mido.Message("note_on", note=midiNote, velocity=midiVelocity)
         self.midiOUT.send(msg)
-
-
+        print("[{}][mute:{}] [midi OUT] : {} ".format(stripId, bool(buttonValue), msg))
 
 
 
@@ -153,9 +167,14 @@ if __name__ == "__main__":
         help="The ip of the OSC server")
     parser.add_argument("--port", type=int, default=8000,
         help="The port the OSC server is listening on")
+    parser.add_argument("--debug", action="store_true", help="Use example datastore")
     args = parser.parse_args()
 
     state = State(args.ip, args.port)
     
-    state.getAll()
+    if args.debug:
+        state._chanData = json.load(open('tests/datastore.json'))
+    else:
+        state.getAll()
+
     state.refreshController()
